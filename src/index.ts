ea2287bebd70b49da9248c9f67535881064afd3a
Giulia Galizoni <giuliagalizoni@gmail.com>
2022-07-26 16:14:02 +0200

    refactoring

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,13 @@ import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
 
-// const numbersCollection = new NumbersCollection([50, 3, -5, 0]);
-// const sorter = new Sorter(numbersCollection);
-// sorter.sort();
+// const numbersCollection = new NumbersCollection([-50, 3, -5, 10]);
+// numbersCollection.sort();
 // console.log(numbersCollection.data);
 
-// const characterCollection = new CharactersCollection('BdfeacGh');
-// const sorter = new Sorter(characterCollection);
-// sorter.sort();
-// console.log(characterCollection.data);
+const characterCollection = new CharactersCollection('BdfeacGh');
+characterCollection.sort();
+console.log(characterCollection.data);
 
 const linkedList = new LinkedList();
 linkedList.add(500);
@@ -19,6 +17,5 @@ linkedList.add(10);
 linkedList.add(-3);
 linkedList.add(4);
 
-const sorter = new Sorter(linkedList);
-sorter.sort();
+linkedList.sort();
 linkedList.print();
