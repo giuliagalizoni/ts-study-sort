6e0ecc1e4d93cc93574a1922267b97dd93297332
Giulia Galizoni <giuliagalizoni@gmail.com>
2022-07-26 15:34:19 +0200

    sorting done

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,24 @@
-console.log('hi there');
+import { Sorter } from './Sorter';
+import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
-const logSomething = () => {
-  console.log('something');
-};
+// const numbersCollection = new NumbersCollection([50, 3, -5, 0]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data);
+
+// const characterCollection = new CharactersCollection('BdfeacGh');
+// const sorter = new Sorter(characterCollection);
+// sorter.sort();
+// console.log(characterCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const sorter = new Sorter(linkedList);
+sorter.sort();
+linkedList.print();
